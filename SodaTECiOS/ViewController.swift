c34c8e2d885db3f82e797e3861256ec2a8eb2962
Jose Fabio Avila Estrada <fabioavila-16@hotmail.com>
2016-09-22 21:39:21 -0700

    First Commit
    Primer commit de la aplicación

---
+++ SodaTECiOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SodaTECiOS
+//
+//  Created by Jose Fabio Avila Estrada on 9/22/16.
+//  Copyright © 2016 Jose Fabio Avila Estrada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
